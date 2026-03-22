315c2f01bf107cd96c62235b7a9e16f852601680
JunarDeseo <47053594+JunarDeseo@users.noreply.github.com>
2026-03-22 21:18:33 +0800

    u-p-0001
    first upload of html project - portfolio

---
+++ gem_.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+// Check for saved user preference
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    body.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') toggleBtn.textContent = '☀️';
+}
+
+toggleBtn.addEventListener('click', () => {
+    let theme = body.getAttribute('data-theme');
+    
+    if (theme === 'dark') {
+        body.removeAttribute('data-theme');
+        localStorage.setItem('theme', 'light');
+        toggleBtn.textContent = '🌙';
+    } else {
+        body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleBtn.textContent = '☀️';
+    }
+});
